68d671e7d916354e5581a4e5cc6187ffa559b5f9
rj7shakya <shakyarajad1@gmail.com>
2020-03-25 22:10:24 +0545

    intro

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const http = require('http');
 
 
-http.createServer((req,res)=>{
-  console.log(req);
+const server = http.createServer((req,res)=>{
+  // console.log(req);
+  // process.exit();
+  res.setHeader('Content-Type','text/html');
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body><h1>Hello from my nodejs server</h1></body>');
+  res.write('</html>');
 });
+
+
+server.listen(3000);
