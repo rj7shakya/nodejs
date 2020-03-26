50ed560f6c296ca97efafb02197d55d32f903c4a
rj7shakya <shakyarajad1@gmail.com>
2020-03-26 09:10:19 +0545

    starting express

--- app.js
+++ app.js
@@ -1,45 +1,24 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-  const method = req.method;
-  const url = req.url;
-  if(url === '/'){
-    res.write('<html>');
-    res.write('<head><title>Enter message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if(url === '/message' && method === 'POST'){
-    const body =[];
-    req.on('data',(chunk)=>{
-      console.log(chunk);
-      body.push(chunk);
-    });
-    req.on('end',()=>{
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt',message,()=>{
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
-      });
-    });
-    
-    
-  }
+const express = require('express');
+const app = express();
 
-
-  // console.log(req);
-  // process.exit();
-  res.setHeader('Content-Type','text/html');
-  res.write('<html>');
-  res.write('<head><title>My first page</title></head>');
-  res.write('<body><h1>Hello from my nodejs server</h1></body>');
-  res.write('</html>');
-  res.end();
+app.use((req,res,next)=>{
+  console.log('In the middleware');
+  next();//alows middle ware to continue to next middleware 
 });
+app.use((req,res,next)=>{
+  console.log('In another middleware');
+  res.send('<h1>Hello from express!!</h1>');
+});
+
+
+
+
+
+
+// const routes = require('./routes');
+const server = http.createServer(app);
 
 
 server.listen(3000);
