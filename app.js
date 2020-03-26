3297e61736484eb98c5c6075816ec9b016a9a21e
rj7shakya <shakyarajad1@gmail.com>
2020-03-26 09:48:18 +0545

    starting express

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product',(req,res,next)=>{
-  res.send('<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add product</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/product',(req,res,next) => {
-  console.log(req.body);
-  res.redirect('/');
-})
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.use('/',(req,res,next)=>{
-  console.log('In another middleware');
-  res.send('<h1>Hello from express!!</h1>');
+app.use((req,res,next)=>{
+  res.status(404).send('<h1>Page not found</h1>')
 });
 
 
