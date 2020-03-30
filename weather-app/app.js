de6128221d930128df6d2b1f321ba0512789cf13
rj7shakya <shakyarajad1@gmail.com>
2020-03-30 11:35:42 +0545

    weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,14 +4,20 @@ const url = 'https://api.darksky.net/forecast/f8f7d9383817711cdd83be3715f181a2/3
 
 request({url:url,json:true},(error,response)=>{
 
+  if(error){
+
+  }else{
+
+  }
+
   // console.log(response.body.currently);
   // console.log(response.body.daily.data[0].summary +" it is currently "+
   // response.body.currently.temperature+" degrees out there. There is "+response.body.currently.precipProbability+"% chances of rain")
 });
 
-const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmFqYWQiLCJhIjoiY2s4ZDQyOXR6MHM2cDNmb2FtZTAzaGoybyJ9.hr-WJPsHOzQKVlQrZ0nikQ&limit=1';
-request({url:mapUrl,json:true},(error,response)=>{
-  const latitude = response.body.features[0].center[1];
-  const latitude = response.body.features[0].center[0];
+// const mapUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmFqYWQiLCJhIjoiY2s4ZDQyOXR6MHM2cDNmb2FtZTAzaGoybyJ9.hr-WJPsHOzQKVlQrZ0nikQ&limit=1';
+// request({url:mapUrl,json:true},(error,response)=>{
+//   const latitude = response.body.features[0].center[1];
+//   const latitude = response.body.features[0].center[0];
   
-});
+// });
