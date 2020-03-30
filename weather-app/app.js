c30a7f4e59d0a025da7bea97026af03498406038
rj7shakya <shakyarajad1@gmail.com>
2020-03-30 14:55:32 +0545

    a weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,19 +4,24 @@ const yargs = require('yargs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-console.log(process.argv[2]);
+// console.log(process.argv[2]);
+const location = process.argv[2];
 
+if (location !== undefined) {
+  geocode(location, (error, data) => {
+    if (error) {
+      return console.log(error);
+    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log('Error', error);
+      console.log('Data', data);
+    });
+  })
 
-// geocode('Boston', (error, data) => {
-//   if(error){
-//     return console.log(error);
-//   }
-//   forecast(data.latitude, data.longitude, (error, forecastData) => {
-//     if(error){
-//       console.log(error);
-//     }
+} else {
+  console.log("Please enter the location");
+}
 
-//     console.log('Error', error);
-//     console.log('Data', data);
-//   });
-// })
