d2166ffa0c2b6683da4412653ebf0aa4525d10d4
rj7shakya <shakyarajad1@gmail.com>
2020-03-30 16:52:49 +0545

    a weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,17 +7,18 @@ const forecast = require('./utils/forecast');
 // console.log(process.argv[2]);
 const location = process.argv[2];
 
-if (location !== undefined) {
-  geocode(location, (error, data) => {
+if (!location) {
+  geocode(location, (error, {latitude,longitude,location}) => {
     if (error) {
       return console.log(error);
     }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+    forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
         console.log(error);
       }
       console.log('Error', error);
-      console.log('Data', data);
+      console.log('Data', forecastData);
+      console.log('Location',location);
     });
   })
 
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -2,14 +2,14 @@ const request = require('request');
 
 const forecast = (latitute, longitude, callback) => {
   const url = 'https://api.darksky.net/forecast/f8f7d9383817711cdd83be3715f181a2/' + latitute + ',' + longitude;
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, {body}) => {
     if (error) {
       callback('unable to connect to weather services.',undefined);
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location',undefined);
     } else {
-      callback(undefined,response.body.daily.data[0].summary + " it is currently " +
-        response.body.currently.temperature + " degrees out there. There is " + response.body.currently.precipProbability + "% chances of rain");
+      callback(undefined,body.daily.data[0].summary + " it is currently " +
+        body.currently.temperature + " degrees out there. There is " + body.currently.precipProbability + "% chances of rain");
     }
   });
 }
