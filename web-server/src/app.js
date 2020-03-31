ba09d40520974f8f1d5fee50e2f77d21c52553b5
rj7shakya <shakyarajad1@gmail.com>
2020-03-31 20:14:13 +0545

    to deploy on heroku

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     name: 'rj'
   })
 })
-app.listen(3000, () => {
-  console.log('Server is up and running on port 3000.')
+app.listen(port, () => {
+  console.log('Server is up and running on port '+port+'.')
 })
