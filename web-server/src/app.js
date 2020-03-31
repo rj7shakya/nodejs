902be32b1b9e334947d67992d191b90ae3410bf0
rj7shakya <shakyarajad1@gmail.com>
2020-03-31 13:09:02 +0545

    web server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,19 +2,25 @@ const express = require('express');
 const path = require('path')
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+  res.render('index',{
+    title: 'Weather ',
+    name :'rj'
+  });
+})
 
-
-app.get('/weather',(req,res)=>{
+app.get('/weather', (req, res) => {
   res.send({
-    location:'Boston',
-    forecast:'90 degrees'
+    location: 'Boston',
+    forecast: '90 degrees'
   })
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
   console.log('Server is up and running on port 3000.')
 })
