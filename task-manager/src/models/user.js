2cb07aafceb224945f0f047cae201290f3dd8645
rj7shakya <shakyarajad1@gmail.com>
2020-04-04 11:36:12 +0545

    authorization

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,8 +40,23 @@ const userSchema = new mongoose.Schema({
         throw new Error('age must be a positive number')
       }
     }
-  }
+  },
+  tokens: [{
+    token:{
+      type: String,
+      required: true
+    }
+  }]
 })
+
+userSchema.methods.generateAuthToken = async function(){
+  const user = this;
+  const token = jwt.sign({_id:user._id.toString()},'thisisnodejs')
+  user.tokens = user.tokens.concat({token})
+  await user.save()
+  return token
+}
+
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email })
@@ -54,6 +70,8 @@ userSchema.statics.findByCredentials = async (email, password) => {
   }
   return user
 }
+
+
 
 //Hash the plain texts password before saving 
 userSchema.pre('save', async function (next) {
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,25 +1,21 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user')
-
+const auth = require('../middleware/auth')
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
   try {
     await user.save()
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (e) {
-    res.status(500).send()
-  }
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -63,9 +59,34 @@ router.patch('/users/:id', async (req, res) => {
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
-    res.send(user)
+    const token = await user.generateAuthToken()
+
+    res.send({ user, token })
   } catch (e) {
     res.status(404).send()
+  }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token
+    })
+    await req.user.save()
+
+    res.status(200).send()
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = []
+    await req.user.save()
+    res.status(200).send()
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
