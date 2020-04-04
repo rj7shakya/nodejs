613ee720b172d3520568f162da8163f431567e59
rj7shakya <shakyarajad1@gmail.com>
2020-04-04 17:22:10 +0545

    api security

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -15,3 +15,16 @@ app.listen(port, () => {
   console.log('Server is up on port' + port);
 })
 
+const Task = require('./models/task')
+const User = require('./models/user')
+
+// const main = async ()=>{
+//   // const task = await Task.findById('5e8858d0d86d9e19a08bd605');
+//   // await task.populate('owner').execPopulate()
+//   // console.log(task.owner)
+//   const user = await User.findById('5e885897cb67c41962b5a06b')
+//   await user.populate('tasks').execPopulate()
+//   console.log(user.tasks)
+// }
+
+// main()
