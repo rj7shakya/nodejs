b0aa20b53ffc24c159a7afdb8771fd31efbfc140
rj7shakya <shakyarajad1@gmail.com>
2020-04-05 13:45:39 +0545

    pagination added

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,13 +18,13 @@ app.listen(port, () => {
 const Task = require('./models/task')
 const User = require('./models/user')
 
-// const main = async ()=>{
-//   // const task = await Task.findById('5e8858d0d86d9e19a08bd605');
-//   // await task.populate('owner').execPopulate()
-//   // console.log(task.owner)
-//   const user = await User.findById('5e885897cb67c41962b5a06b')
-//   await user.populate('tasks').execPopulate()
-//   console.log(user.tasks)
-// }
+const main = async ()=>{
+  // const task = await Task.findById('5e89796993db17601c58d537');
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner)
+  const user = await User.findById('5e89793993db17601c58d535')
+  await user.populate('tasks').execPopulate()
+  // console.log(user.tasks)
+}
 
-// main()
+main() 
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -17,17 +17,40 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=20
+//GET /tasks?sortBy=createdAt_(:)asc(desc)
+
 router.get('/tasks', auth, async (req, res) => {
+  const sort = {}
+  const match = {}
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true'
+  }
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(':')
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+  }
+
   try {
-    const tasks = await Task.find({owner:req.user._id});
-    // await req.user.populate('tasks').execPopulate()
-    res.status(201).send(tasks)
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+        sort
+      }
+    }).execPopulate()
+    res.status(201).send(req.user.tasks)
   } catch (e) {
     res.status(500).send()
   }
 })
 
 router.get('/tasks/:id', auth, async (req, res) => {
+
   const _id = req.params.id;
   try {
     const task = await Task.findOne({ _id, owner: req.user._id })
@@ -63,9 +86,9 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 })
 
 
-router.delete('/tasks/:id',auth, async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findOneAndDelete({_id:req.params.id,owner:req.user._id})
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
     if (!task) {
       return res.status(404).send()
     }
