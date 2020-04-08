7303be8e58d435bf7127c45a081f1f25b933e132
rj7shakya <shakyarajad1@gmail.com>
2020-04-09 04:28:04 +0545

    style added

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -3,63 +3,70 @@ const socket = io()
 //Elements 
 const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = $messageForm.querySelector('input')
-const $messageFormButton = $messageForm.querySelector('button') 
-const $shareLocation = document.querySelector('#send-location') 
+const $messageFormButton = $messageForm.querySelector('button')
+const $shareLocation = document.querySelector('#send-location')
 const $messages = document.querySelector('#messages')
 
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
 
-socket.on('message',(message)=>{
+
+//options
+const { username, room } = Os.parse(location.search, { ignoreQueryPrefix: true })
+
+socket.on('message', (message) => {
   console.log(message)
-  const html = Mustache.render(messageTemplate,{
-    message
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
   })
-  $messages.insertAdjacentHTML('beforeend',html)
+  $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage',(url)=>{
-  const html = Mustache.render(locationTemplate,{
-    url
+socket.on('locationMessage', (message) => {
+  const html = Mustache.render(locationTemplate, {
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
   })
-  $messages.insertAdjacentHTML('beforeend',html)
-  // console.log(url)
+  $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#message-form').addEventListener('submit',(e)=>{
+document.querySelector('#message-form').addEventListener('submit', (e) => {
   e.preventDefault()
   //disable
-  $messageFormButton.setAttribute('disabled','disabled')
+  $messageFormButton.setAttribute('disabled', 'disabled')
 
   const message = e.target.elements.message.value
 
-  socket.emit('sendMessage',message,(error)=>{
+  socket.emit('sendMessage', message, (error) => {
     //enable
     $messageFormButton.removeAttribute('disabled')
     $messageFormInput.value = ''
     $messageFormInput.focus()
 
-    if(error){
+    if (error) {
       return console.log(error)
     }
     console.log('the message was delivered')
   })
 })
 
-$shareLocation.addEventListener('click',()=>{
-  if(!navigator.geolocation){
+$shareLocation.addEventListener('click', () => {
+  if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser')
   }
-  $shareLocation.setAttribute('disabled','disabled')
-  
-  navigator.geolocation.getCurrentPosition((position)=>{
-    socket.emit('sendLocation',{
-      latitude:position.coords.latitude,
-      longitude:position.coords.longittude
-    },()=>{
+  $shareLocation.setAttribute('disabled', 'disabled')
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longittude
+    }, () => {
       console.log('Location shared!')
       $shareLocation.removeAttribute('disabled')
     })
   })
 })
+
+socket.emit('join', { username, room })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -3,48 +3,47 @@ const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocation } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let msg = "Welcome"
-
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
   console.log('New Websocket connection')
 
-  socket.emit('message','Welcome!');
-  socket.broadcast.emit('message','A new user has joined')
+  socket.emit('message', generateMessage('Welcome!'));
+  socket.broadcast.emit('message', generateMessage('A new user has joined'))
 
-  socket.on('sendMessage',(message,callback)=>{
+  socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
-    if(filter.isProfane(message)){
+    if (filter.isProfane(message)) {
       return callback('Profanity is not allowed!')
     }
 
-    io.emit('message',message)
+    io.emit('message', generateMessage(message))
     callback('delivered')
   })
 
-  socket.on('sendLocation',(data,callback)=>{
+  socket.on('sendLocation', (data, callback) => {
     // if(data.latitude == undefined || data.longitude==undefined){
     //   return callback('Location cant be send')
     // }
-    io.emit('locationMessage',`https://google.com/maps?q=${data.latitude},${data.longitude}`)
+    io.emit('locationMessage', generateLocation(`https://google.com/maps?q=${data.latitude},${data.longitude}`))
     callback()
   })
 
-  socket.on('disconnect',()=>{
-    io.emit('message','A user has left')
+  socket.on('disconnect', () => {
+    io.emit('message', generateMessage('A user has left'))
   })
 })
 
 
-server.listen(3000, ()=>{
+server.listen(3000, () => {
   console.log(`Server is runnning on port ${port}`)
 })
